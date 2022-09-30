739f2b3b307b268e5004a7fc9e645155316a569c
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-30 02:43:58 +0200

    bandiera per lingua +alcune correzioni errori

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -11,7 +11,8 @@
       <li v-for="result in apiResults" :key="result.id">
         <div class="title">Titolo: {{result.title}}</div>
         <div class="title">Titolo originale: {{result.original_title}}</div>
-        <div class="title">Lingua originale: {{result.original_language}}</div>
+        <div class="title">Lingua originale: <span><img :src="getLanguageFlag(result)"
+              :alt="result.original_language"></span></div>
         <div class="title">Voto: {{result.vote_average}}</div>
       </li>
     </ul>
@@ -34,6 +35,38 @@ export default {
     getSearchTitle() {
       console.log('Ricerca su: ', this.inputText);
       this.$emit('search', this.inputText)
+    },
+    getLanguageFlag(result) {
+
+      let langFlag = '';
+
+      switch (result.original_language) {
+        case 'ja':
+          langFlag = 'jp'
+          break;
+        case 'en':
+          langFlag = 'gb-eng'
+          break;
+        case 'cs':
+          langFlag = 'cz'
+          break;
+        case 'da':
+          langFlag = 'dk'
+          break;
+        // Lingue indiane: hindi, telugu
+        case 'hi', 'te':
+          langFlag = 'in'
+          break;
+
+        default:
+          langFlag = result.original_language
+          break;
+      }
+
+
+      const flagUrl = `https://flagcdn.com/16x12/${langFlag}.png`
+
+      return flagUrl
     }
   }
 }
