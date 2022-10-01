7ff3c287ec0ee77c78ebf322ca2cd81c025ca4ba
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-01 12:28:43 +0200

    Utilizzo di un SearchComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="container pt-5">
 
-    <div class="search-area">
-      <input type="text" placeholder="Inserire titolo film" v-model="inputText" @keyup.enter="getSearchTitle">
-      <button @click="getSearchTitle">Cerca</button>
-    </div>
+    <!-- Utilizzo di un componente per la ricerca -->
+    <SearchComponent @search="getSearchText" />
 
     <!-- Elenco film risultato chiamata axios -->
     <ul>
@@ -21,54 +19,56 @@
 </template>
 
 <script>
+import SearchComponent from '@/components/utils/SearchComponent.vue';
 export default {
-  name: 'MainComponent',
+  name: "MainComponent",
   data() {
     return {
-      inputText: ''
-    }
+      inputText: ""
+    };
   },
   props: {
     apiResults: Array
   },
   methods: {
     getSearchTitle() {
-      console.log('Ricerca su: ', this.inputText);
-      this.$emit('search', this.inputText)
+      console.log("Ricerca su: ", this.inputText);
+      this.$emit("search", this.inputText);
     },
     getLanguageFlag(result) {
-
-      let langFlag = '';
-
+      let langFlag = "";
       // Controllo i casi in cui il codice lingua non coincide col codice bandiera
       switch (result.original_language) {
-        case 'ja':
-          langFlag = 'jp'
+        case "ja":
+          langFlag = "jp";
           break;
-        case 'en':
-          langFlag = 'gb-eng'
+        case "en":
+          langFlag = "gb-eng";
           break;
-        case 'cs':
-          langFlag = 'cz'
+        case "cs":
+          langFlag = "cz";
           break;
-        case 'da':
-          langFlag = 'dk'
+        case "da":
+          langFlag = "dk";
           break;
         // Lingue indiane: hindi, telugu
-        case 'hi', 'te':
-          langFlag = 'in'
+        case "hi", "te":
+          langFlag = "in";
           break;
-
         default:
-          langFlag = result.original_language
+          langFlag = result.original_language;
           break;
       }
-
       // Attribuisco il codice bandiera esatto e restituisco l'url
-      const flagUrl = `https://flagcdn.com/16x12/${langFlag}.png`
-      return flagUrl
+      const flagUrl = `https://flagcdn.com/16x12/${langFlag}.png`;
+      return flagUrl;
+    },
+    getSearchText(textToSearch) {
+      console.log("Nel main ottengo: ", textToSearch);
+      this.$emit("search", textToSearch);
     }
-  }
+  },
+  components: { SearchComponent }
 }
 </script>
 
---
+++ src/components/utils/SearchComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search-area">
+        <input type="text" placeholder="Inserire elemento da cercare" v-model="inputText" @keyup.enter="getSearchTitle">
+        <button @click="getSearchTitle">Cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchComponent',
+    data() {
+        return {
+            inputText: ""
+        };
+    },
+    methods: {
+
+        getSearchTitle() {
+            console.log("Ricerca su: ", this.inputText);
+            this.$emit("search", this.inputText);
+        },
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
