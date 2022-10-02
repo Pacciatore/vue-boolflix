32027ae23f0390293f1dacf386e7b458c7dfcfb1
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-03 00:43:32 +0200

    gestione mancanza poster

--- src/components/utils/PosterComponent.vue
+++ src/components/utils/PosterComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 
-        <img :src="getPosterPath()" :alt="posterName">
+        <img :src="getPosterPath()" :alt="posterName" @error="setUnavailablePoster($event)">
 
     </div>
 </template>
@@ -22,6 +22,9 @@ export default {
     methods: {
         getPosterPath() {
             return this.posterPath = `https://image.tmdb.org/t/p/${this.posterWidth}/${this.posterEndPath}`;
+        },
+        setUnavailablePoster(event) {
+            event.target.src = 'https://www.metrorollerdoors.com.au/wp-content/uploads/2018/02/unavailable-image-300x225.jpg'
         }
     }
 }
