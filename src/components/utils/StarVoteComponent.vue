dfc383e124ee05288c7ed5d345bd6461f6f2e10c
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-03 00:35:32 +0200

    creazione componente per voto in stelle

---
+++ src/components/utils/StarVoteComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="d-flex">
+
+        <font-awesome-icon icon="fa-solid fa-star" />
+
+        <font-awesome-icon v-if="voteMaxFive > 1" icon="fa-solid fa-star" />
+
+        <font-awesome-icon v-if="voteMaxFive > 2" icon="fa-solid fa-star" />
+
+        <font-awesome-icon v-if="voteMaxFive > 3" icon="fa-solid fa-star" />
+
+        <font-awesome-icon v-if="voteMaxFive > 4" icon="fa-solid fa-star" />
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StarVoteComponent',
+    data() {
+        return {
+            voteMaxFive: 1
+        }
+    },
+    props: {
+        vote: Number
+    },
+    created() {
+        this.voteToMaxFive();
+    },
+    methods: {
+        voteToMaxFive() {
+            this.voteMaxFive = Math.ceil(this.vote / 2);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+.fa-star {
+    color: $tr-star-yellow;
+}
+</style>
