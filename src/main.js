bb5b94b0ad899c4ecfa5cb6567dbb3d7c38124a9
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-03 10:27:37 +0200

    installazione regular icons

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fas)
 
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(far)
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
